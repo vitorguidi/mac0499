7f9d7226887b2484b3584c4a33f350f779d03ffd
Vitor Guidi <vitorguidi@gmail.com>
2023-08-19 17:55:00 -0300

    Trying to print exception

--- chaos/test_scripts/spira_api_flood.py
+++ chaos/test_scripts/spira_api_flood.py
@@ -31,7 +31,7 @@ def flood_and_log():
         except(requests.ConnectionError):
             f.write("connection failed\n")
         except(requests.HTTPError) as e:
-            f.write(e.errno)
+            f.write(e)
 
 def get_token():
     
